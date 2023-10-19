1d5374cdf4c2d8360f19a217867eafe995beda49
Mark Biek <mark.biek@automattic.com>
2023-10-19 14:36:19 -0400

    Basic script to display a list of GH column cards as markdown

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
-const readline = require("readline");
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
+import chalk from "chalk";
+import { input, confirm } from "@inquirer/prompts";
+
+function error(message) {
+	console.log("ERROR: " + chalk.red(message));
+
+	return false;
+}
+
+function success(message) {
+	console.log("SUCCESS: " + chalk.green(message));
+
+	return true;
+}
+
+function warning(message) {
+	console.log("WARNING: " + chalk.yellow(message));
+
+	return true;
+}
+
+function info(message) {
+	console.log(chalk.blue(message));
+
+	return true;
+}
 
 // Read the GitHub token from the system environment
 const GHTOKEN = process.env.GHTOKEN;
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-function promptForColumnURL() {
-	rl.question(
-		"Enter the GitHub column URL (e.g., https://github.com/orgs/Automattic/projects/148#column-<column-id>): ",
-		async (url) => {
-			// Extract the column ID from the URL
-			const columnIdMatch = url.match(/#column-(\d+)$/);
-
-			if (columnIdMatch) {
-				const columnId = columnIdMatch[1];
-				try {
-					const columnData = await fetchColumnData(columnId);
-					console.log(`Column Name: ${columnData.name}`);
-
-					rl.question("Is this the correct column? (yes/no): ", (answer) => {
-						if (answer.toLowerCase() === "yes") {
-							// User confirmed the column, proceed with further processing
-							rl.close();
-							process.exit(0);
-						} else {
-							// User did not confirm the column, prompt again
-							promptForColumnURL();
-						}
-					});
-				} catch (error) {
-					console.error(error);
-					rl.close();
-					process.exit(1);
-				}
-			} else {
-				console.error(
-					"Invalid column URL. Please make sure it includes #column-<column-id>."
-				);
-				promptForColumnURL();
-			}
-		}
-	);
+if (!GHTOKEN) {
+	error("Please set the GHTOKEN environment variable.");
+	process.exit(1);
 }
 
-async function fetchColumnData(columnId) {
-	const url = `https://api.github.com/projects/columns/${columnId}`;
+async function promptForColumnURL() {
+	const url = await input({
+		message:
+			"Enter the GitHub column URL (e.g., https://github.com/orgs/Automattic/projects/148#column-<column-id>):",
+	});
+	// Extract the column ID from the URL
+	const columnIdMatch = url.match(/#column-(\d+)$/);
+
+	if (columnIdMatch) {
+		const columnId = columnIdMatch[1];
+		const columnData = await fetchColumnData(columnId);
+		info(`Column Name: "${columnData.name}"`);
+
+		const answer = await confirm({
+			default: true,
+			message: "Is this the correct column? ",
+		});
+		if (!answer) {
+			error(
+				"Aborting. Please run the script again with the correct column URL."
+			);
+			process.exit(1);
+		}
+
+		return columnId;
+	} else {
+		error(
+			"Invalid column URL. Please make sure it includes #column-<column-id>."
+		);
+		process.exit(1);
+	}
+}
+
+async function queryGithubAPI(url) {
 	const headers = {
 		Accept: "application/vnd.github+json",
 		Authorization: `Bearer ${GHTOKEN}`,
@@ -62,7 +81,7 @@ async function fetchColumnData(columnId) {
 			return data;
 		} else {
 			throw new Error(
-				`Failed to fetch column data from GitHub API: ${response.status} ${response.statusText}`
+				`Failed to fetch data from GitHub API: ${response.status} ${response.statusText}`
 			);
 		}
 	} catch (error) {
@@ -70,5 +89,43 @@ async function fetchColumnData(columnId) {
 	}
 }
 
-// Start by prompting for the column URL
-promptForColumnURL();
+async function fetchColumnData(columnId) {
+	const url = `https://api.github.com/projects/columns/${columnId}`;
+
+	return await queryGithubAPI(url);
+}
+
+async function fetchColumnCards(columnId) {
+	const url = `https://api.github.com/projects/columns/${columnId}/cards`;
+
+	return await queryGithubAPI(url);
+}
+
+async function main() {
+	const columnId = await promptForColumnURL();
+	const cards = await fetchColumnCards(columnId);
+
+	let markdown = "";
+	for (let card of cards) {
+		if (!card.note) {
+			continue;
+		}
+
+		let cleanNote = "";
+
+		// Replace Phabricator shortcodes with a markdown link
+		cleanNote = card.note.replace(
+			/D(\d+)-code/g,
+			"[D$1](https://code.a8c.com/D$1)"
+		);
+
+		// Remove any newlines from the note
+		cleanNote = cleanNote.replace(/\n|\r/g, " ").replace(/\s{2,}/, " ");
+
+		markdown += `- ${cleanNote}\n`;
+	}
+
+	console.log(markdown);
+}
+
+main();
